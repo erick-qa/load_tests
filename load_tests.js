fab854819f9761871d98102bb304f8edb8fa359f
erick-qa <erick_gtr9@hotmail.com>
2024-12-18 16:22:44 -0300

    Update Test

--- load_tests.js
+++ load_tests.js
@@ -10,27 +10,21 @@ export function handleSummary(data) {
   };
 }
 
-// Opções do teste de carga
 export let options = {
-  vus: 500, // número de usuários virtuais
-  duration: '5m', // duração do teste (5 minutos)
+  vus: 500,
+  duration: '5m',
 };
 
-// Função principal que será executada por cada usuário virtual
 export default function () {
-  // Teste 1: Listar usuários (consulta)
   let listUsersResponse = http.get('https://reqres.in/api/users?page=1');
   
-  // Verifique se a resposta foi bem-sucedida (status 200)
   check(listUsersResponse, {
     'status is 200': (r) => r.status === 200,
   });
 
-  // Teste 2: Validar que o corpo da resposta contém dados de usuários
   check(listUsersResponse, {
     'response body contains users': (r) => r.json('data').length > 0,
   });
 
-  // Espera de 1 segundo entre as ações (simula o tempo de uso real)
   sleep(1);
 }
